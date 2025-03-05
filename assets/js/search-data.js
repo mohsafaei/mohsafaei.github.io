a9f9d59b979dbab0067ce6a308c7ee0620f113d8
mohsafaei <130284531+mohsafaei@users.noreply.github.com>
2025-03-05 14:54:35 +0000

    Deploying to gh-pages from @ mohsafaei/mohsafaei.github.io@fa25a817edf0c8ddd29e53a2148016ad696c97fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,14 +490,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61.%73%61%66%61%65%69@%75%74.%61%63.%69%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/mohsafaei", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0002-4917-6464", "_blank");
         },
       },{
         id: 'social-rss',
@@ -511,14 +518,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=jD_-4JcAAAAJ&hl", "_blank");
         },
       },{
       id: 'light-theme',
