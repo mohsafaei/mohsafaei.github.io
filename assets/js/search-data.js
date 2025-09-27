fd22a87eb4b0703546ea370709afe24cffc257a7
mohsafaei <130284531+mohsafaei@users.noreply.github.com>
2025-09-27 19:44:58 +0000

    Deploying to gh-pages from @ mohsafaei/mohsafaei.github.io@d3946a91870d129b94956dff2dbc352fe50aef6d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -90,17 +90,7 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/projects/10_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
@@ -131,6 +121,26 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-cad-drawings",
+          title: 'CAD drawings',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/project_CAD/";
+            },},{id: "projects-deep-learning",
+          title: 'Deep Learning',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/project_DL/";
+            },},{id: "projects-machine-learning",
+          title: 'Machine Learning',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/project_ML/";
+            },},{id: "projects-reinforcement-learning",
+          title: 'Reinforcement Learning',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/project_RL/";
             },},{id: "teaching-non-linear-solid-mechanics",
           title: 'Non-linear Solid Mechanics',
           description: "with background image",
