e240e28c907f5a75efd07d8b7f3cf4ae8d26a361
mohsafaei <130284531+mohsafaei@users.noreply.github.com>
2025-09-28 08:48:12 +0000

    Deploying to gh-pages from @ mohsafaei/mohsafaei.github.io@4b6b94ef6e6590ae3f4a37847c4edec58185927e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -86,42 +86,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-ve-officially-started-my-new-role-teaching-english-as-a-second-language-to-teenagers-and-adults-at-the-nogolearn-institute-can-t-wait-to-help-my-students-grow",
           title: '📣 I’ve officially started my new role teaching English as a second language...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/10_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-cad-drawings",
+          section: "News",},{id: "projects-cad-drawings",
           title: 'CAD drawings',
           description: "",
           section: "Projects",handler: () => {
